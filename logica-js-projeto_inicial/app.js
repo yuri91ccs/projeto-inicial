a7bd5c0210d67fb3e3a2fe68af9ecbadfdfdc793
Yuri Carlos <yuri91.ccs@gmail.com>
2025-02-07 13:39:57 -0300

    Desenvolvendo 2

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,11 @@
 let numeroSecreto = 10
-let chute = prompt
+let chute
 
 console.log ("yuri");
 alert ('Olá! Bem vindo ao jogo do número secreto!');
-prompt ('Digite um número de 1 a 10.')
+chute = prompt ('Digite um número de 1 a 10.')
 
-if (chute = numeroSecreto) {
-    alert ('Acertou!')     
+if (chute == numeroSecreto) {
+    alert ('Acertou!')
 } 
+else (alert ('Você errou o numero secreto. Tente novamente.'))
